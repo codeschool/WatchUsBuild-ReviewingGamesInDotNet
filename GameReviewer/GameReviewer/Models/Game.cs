87b29ecf6b79a8377abd44f07dbc6a5b5686e136
Eric J Fisher <EricJFisher@Live.com>
2016-06-10 02:35:52 -0400

    Removed the First or Default and used a foreach to make things simplier for those watching.

--- GameReviewer/GameReviewer/Models/Game.cs
+++ GameReviewer/GameReviewer/Models/Game.cs
@@ -7,7 +7,19 @@ namespace GameReviewer.Models
     {
         public string Name { get; set; }
         public string Description { get; set; }
-        public double Rating { get { if (Reviews.Any()) { return Reviews.Average(e => e.Rating); } else { return 0; } } }
+        public double Rating {
+            get
+            {
+                if (Reviews.Any())
+                {
+                    return Reviews.Average(e => e.Rating);
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
 
         public List<Review> Reviews { get; set; } = new List<Review>();
 
@@ -18,7 +30,12 @@ namespace GameReviewer.Models
         
         public static Game Get(string name)
         {
-            return GlobalVariables.Games.FirstOrDefault(e => e.Name == name);
+            foreach(var game in GlobalVariables.Games)
+            {
+                if (game.Name == name)
+                    return game;
+            }
+            return null;
         }
         
         public static void Create(Game game)
