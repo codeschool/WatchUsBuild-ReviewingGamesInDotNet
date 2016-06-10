3a0f900f7b3248a5bea09442ec90b68e3e58a52a
Eric J Fisher <EricJFisher@Live.com>
2016-06-09 23:49:05 -0400

    Added References for code generation and added models

---
+++ GameReviewer/GameReviewer/GlobalVariable.cs
@@ -0,0 +1,10 @@
+﻿using GameReviewer.Models;
+using System.Collections.Generic;
+
+namespace GameReviewer
+{
+    public class GlobalVariable
+    {
+        public List<Game> Games { get; set; } = new List<Game>();
+    }
+}
---
+++ GameReviewer/GameReviewer/Models/Game.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace GameReviewer.Models
+{
+    public class Game
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Rating { get; set; }
+
+        public List<Review> Reviews { get; set; } = new List<Review>();
+    }
+}
