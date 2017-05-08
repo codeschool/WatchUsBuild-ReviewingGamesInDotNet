472d1b95bb0db34873d8887eeec638780c992ac6
Eric J Fisher <EricJFisher@Live.com>
2017-05-08 13:41:16 -0400

    Updated Repo to .NET Core 1.1
    This is a major overhaul of the entire project requiring us to replace
    the project and solution files as well as making changes to `Program.cs`
    and `Startup.cs` for those who made their own changes then merge this
    will likely not have a good time. (but needs doing with the `xproj` file
    being effectively killed)

---
+++ GameReviewer/Controllers/GameController.cs
@@ -0,0 +1,41 @@
+﻿using GameReviewer.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameReviewer.Controllers
+{
+    public class GameController : Controller
+    {
+        // GET: Game
+        public ActionResult Index()
+        {
+            return View(Game.GetAll());
+        }
+
+        // GET: Game/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Error()
+        {
+            return View();
+        }
+
+        // POST: Game/Create
+        [HttpPost]
+        public ActionResult Create(Game game)
+        {
+            try
+            {
+                Game.Create(game);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ GameReviewer/Controllers/Review.cs
@@ -0,0 +1,23 @@
+﻿using GameReviewer.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameReviewer.Controllers
+{
+    public class ReviewController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Create(string game)
+        {
+            ViewData["Game"] = game;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Review review, string gameName)
+        {
+            var game = Game.Get(gameName);
+            game.AddReviewToGame(review);
+            return RedirectToAction("Index", "Game");
+        }
+    }
+}
